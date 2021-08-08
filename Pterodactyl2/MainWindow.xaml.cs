74e7308f93633c538e4aabbec0d7a277297f6844
parkero2 <parkero2@stu.otc.school.nz>
2021-08-08 14:41:10 +1200

    Initial commit

---
+++ Pterodactyl2/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+
+using Sharpdactyl;
+using Sharpdactyl.Models.Client;
+
+namespace Pterodactyl2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    public class databaseCreator
+    {
+        SQLiteConnection dbConnection;
+        SQLiteCommand command;
+        string sqlCommand;
+        string dbPath = System.Environment.CurrentDirectory + "\\DB";
+        string dbFilePath;
+        public void createDB()
+        {
+            if (!string.IsNullOrEmpty(dbPath) && !Directory.Exists(dbPath))
+            {
+                Directory.CreateDirectory(dbPath);
+                dbFilePath = dbPath + "\\servers.db";
+                if (!System.IO.File.Exists(dbFilePath))
+                {
+                    SQLiteConnection.CreateFile(dbFilePath);
+                }
+            }
+        }
+
+        public String createDBConnection()
+        {
+            string strCon = string.Format("Date Source={0}", dbFilePath);
+            dbConnection = new SQLiteConnection(strCon);
+            dbConnection.Open();
+            command = dbConnection.CreateCommand();
+            return strCon;
+        }
+    }
+}
